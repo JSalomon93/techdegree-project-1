fd708cab4ff1430ec52fbf2df9fc3c8cb2b1ccbb
JSalomon93 <97906916+JSalomon93@users.noreply.github.com>
2022-01-17 18:19:47 -0500

    added comments

--- guessing_game.py
+++ guessing_game.py
@@ -16,17 +16,21 @@ correct_list = []
 
 
 def start_game():
-
+    # define number range, set starting guesses equal to zero and create a list for this rounds number of guesses
     winning_value = random.randint(1,100)
     num_guesses = 0
     current_list = []
     
+    # header to welcome the player
     print("""
     *********************************************************************************************
     ************************** Welcome to the number guessing game! ***************************** 
     *********************************************************************************************
     """)
     
+    # while loop to ensure value of guess is within range
+    # notifies user if their selection is higher or lower than the correct value
+    # if the user enters an invalid value, it prompts them to re-enter
     while True:
         print("Please make a guess: ")
         
@@ -45,7 +49,7 @@ def start_game():
         except ValueError:
             print("Please enter a valid number between 1 and 100 ")
             num_guesses += 1
-        
+    # congratulates the user for a correct answer and returns the number of guesses it took them    
     print("Congratulations, you were able to guess the mystery number in {} tries!".format(num_guesses))
     current_list.append(num_guesses)
     correct_list.extend(current_list)
@@ -55,7 +59,7 @@ start_game()
 
 
 while True:
-     
+    #makes calculations based off of previous rounds played 
     print("Your previous scores are: ")
     
     for scores in correct_list:
@@ -65,7 +69,8 @@ while True:
     num_median = round(median_low(correct_list))
     num_mode = round(mode(correct_list))
     
-    print("\nYour Mean score is now: {} \nYour Median score is now : {} \nYour Mode score is now: {}".format(num_mean, num_median, num_mode))    
+    print("\nYour Mean score is now: {} \nYour Median score is now : {} \nYour Mode score is now: {}".format(num_mean, num_median, num_mode))
+    #prompts player to see if they would like to continue playing
     print("\nWould you like to play again? Y/N?")
     
     try:
